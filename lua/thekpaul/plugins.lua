c489f19b2405df1016c2ab9cbd316775ac1477ee
Paul Kim <44695374+thekpaul@users.noreply.github.com>
2025-07-22 06:46:22 +0900

    FEAT: Lazy.nvim Plugin Manager Bootstrapping
    Installs (if necessary) and bootstraps the Lazy.nvim plugin manager
    after searching for and checking the plugin configuration modules
    directory to determine if plugin manager bootstrapping is necessary.
    If issues are found during the directory search and check stage,
    this module prouduces a warning message and gracefully returns without
    terminating or otherwise producing errors.
    
    Signed-off-by: Paul Kim <44695374+thekpaul@users.noreply.github.com>

--- lua/thekpaul/plugins.lua
+++ lua/thekpaul/plugins.lua
@@ -7,3 +7,50 @@ If no plugin configurations are found, this module produces a warning message
 and gracefully returns without terminating or otherwise producing errors.
 --]=]
 
+-- Absolute path to plugin configuration modules directory
+local plug_abs = vim.fs.abspath(vim.fn.stdpath("config") .. "/lua/plugins")
+-- Relative path to plugin configuration modules directory
+local plug_rel = vim.fs.relpath(vim.fn.stdpath("config"), plug_abs)
+
+-- Check for plugin configuration files in designated directory
+local iter = vim.fs.dir(plug_abs)
+if not iter then
+	vim.notify("Plugin configurations path ./" .. plug_rel .. "/ is invalid.",
+	           vim.log.levels.WARN)
+	return
+elseif iter() == nil then
+	vim.notify("No plugin configurations found in ./" .. plug_rel .. "/",
+	           vim.log.levels.WARN)
+	return
+end
+
+-- Bootstrap LAZY.nvim as a native Lua plugin manager
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"--branch=stable", -- latest stable release
+		lazyrepo,
+		lazypath
+	})
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup("plugins", {
+	-- LAZY.nvim options
+	change_detection = {
+		notify = false -- Don't notify when plugin configurations change
+	}
+})
