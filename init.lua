429e3e2b9b4af48f2525f7f04f55be771d379089
Paul Kim <44695374+thekpaul@users.noreply.github.com>
2025-11-22 07:34:25 +0900

    CHORE: Disable All Neovim Plugins in VS Code
    Uses the `vim.g.vscode` variable provided in VS Code environments by the
    VSCode-Neovim plugin to detect if Neovim is attached to VS Code.
    If it is, then suppress all Neovim plugins managed by Lazy.nvim.
    
    This is a temporary operation to ensure that Neovim plugins do not mess
    with VS Code sessions.
    Future commits on this branch will be more granular in deciding which
    plugins to allow in VS Code (if any), as well as setting different
    options and/or keymaps in VS Code.
    
    Signed-off-by: Paul Kim <44695374+thekpaul@users.noreply.github.com>

--- init.lua
+++ init.lua
@@ -4,4 +4,6 @@ INIT.lua: Configuration Entry Point for Neovim in Lua
 
 require("thekpaul.options")
 require("thekpaul.keymaps")
-require("thekpaul.plugins")
+if not vim.g.vscode then
+	require("thekpaul.plugins")
+end
